fa6ea08f3b3eacdbe57e4c907417cf347437b1a0
Daniel Choi <dchoi87@gmail.com>
2022-01-25 22:58:40 -0800

    Tags & Priority

---
+++ src/components/tasks/Priority.js
@@ -0,0 +1,12 @@
+const Priority = () => {
+  return (
+    <div className="tasks-priority">
+      <div className="tasks-priority__btn tasks__btn">No Priority</div>
+      <div className="tasks-priority__btn tasks__btn">P2</div>
+      <div className="tasks-priority__btn tasks__btn">P3</div>
+      <div className="tasks-priority__btn tasks__btn">Urgent</div>
+    </div>
+  )
+}
+
+export default Priority;
--- src/components/tasks/SideBar.js
+++ src/components/tasks/SideBar.js
@@ -1,5 +1,7 @@
 import Summary from './Summary';
 import ViewOptions from './ViewOptions';
+import Priority from './Priority';
+import Tags from './Tags';
 
 const SideBar = () => {
   return (
@@ -25,11 +27,11 @@ const SideBar = () => {
       <div className="tasks-sidebar__section">
         <div className="tasks-sidebar__header">
           <span>By Priority</span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
           </svg>
         </div>
-        {/* <Summary /> */}
+        <Priority />
       </div>
       <div className="tasks-sidebar__section">
         <div className="tasks-sidebar__header">
@@ -52,11 +54,11 @@ const SideBar = () => {
       <div className="tasks-sidebar__section">
         <div className="tasks-sidebar__header">
           <span>By Tags</span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
           </svg>
         </div>
-        {/* <Summary /> */}
+        <Tags />
       </div>
     </div>
   )
