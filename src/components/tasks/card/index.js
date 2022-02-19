55575391bbe371c1585643fda5c9ffdec3fded3a
Daniel Choi <dchoi87@gmail.com>
2022-02-19 00:47:13 -0800

    Cards CSS Module

--- src/components/tasks/card/index.js
+++ src/components/tasks/card/index.js
@@ -1,65 +1,64 @@
 import React from "react";
+import classNames from "classnames";
+import {
+  Alarm,
+  PersonCircle,
+  Tag,
+  Paperclip,
+  Circle,
+} from "react-bootstrap-icons";
 
-import { Alarm, PersonCircle, Tag, Paperclip } from "react-bootstrap-icons";
+import styles from "./index.module.css";
 
 const Cards = ({ showDesktopView, viewOption }) => {
   const shouldShowFullTags = showDesktopView && viewOption !== "grid";
 
   return (
-    <button className="tasks-cards__item tasks__btn">
-      <div className="tasks-cards__checkbox">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-circle"
-          viewBox="0 0 16 16"
-        >
-          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-        </svg>
+    <button className={styles.container}>
+      <div className="select">
+        <Circle />
       </div>
-      <div className="tasks-cards__content">
-        <div className="tasks-cards__section--top">
-          <div className="tasks-cards__subsection--left">
-            <div className="tasks-cards__title">
+      <div className={styles.content}>
+        <div className={styles.sectionTop}>
+          <div className={styles.subsectionLeft}>
+            <div className={styles.title}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit
             </div>
           </div>
-          <div className="tasks-cards__subsection--right">
-            <div className="tasks-cards__priority tasks-cards__priority--urgent">
+          <div className={styles.subsectionRight}>
+            <div className={classNames(styles.priority, styles.urgent)}>
               Urgent
             </div>
           </div>
         </div>
-        <div className="tasks-cards__section--middle">
-          <div className="tasks-cards__description">Some Description Text</div>
+        <div className="sectionMiddle">
+          <div className={styles.description}>Some Description Text</div>
         </div>
-        <div className="tasks-cards__section--bottom">
-          <div className="tasks-cards__subsection--left">
-            <div className="tasks-cards__duedate">
+        <div className={styles.sectionBottom}>
+          <div className={styles.subsectionLeft}>
+            <div className={styles.duedate}>
               <Alarm />
               <span>12-17-2021</span>
             </div>
-            <div className="tasks-cards__patient">
+            <div className={styles.patient}>
               <PersonCircle />
               <span>Daniel Choi</span>
             </div>
           </div>
-          <div className="tasks-cards__subsection--right">
+          <div className={styles.subsectionRight}>
             {shouldShowFullTags ? (
               <>
-                <div className="tasks-cards__tag">Form Submission</div>
-                <div className="tasks-cards__tag">Doc Review</div>
-                <div className="tasks-cards__tag">+2</div>
+                <div className={styles.tag}>Form Submission</div>
+                <div className={styles.tag}>Doc Review</div>
+                <div className={styles.tag}>+2</div>
               </>
             ) : (
-              <div className="tasks-cards__tag tasks-cards__tag--mobile">
+              <div className={classNames(styles.tag, styles.consolidated)}>
                 <Tag />
                 <span>+3</span>
               </div>
             )}
-            <div className="tasks-cards__attachment">
+            <div className={styles.attachment}>
               <Paperclip />
             </div>
           </div>
