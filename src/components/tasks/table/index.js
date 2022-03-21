49c81415e119fa2de6d41bd44e80f47255dc86c9
Daniel Choi <dchoi87@gmail.com>
2022-03-20 23:17:18 -0700

    More Table Updates

--- src/components/tasks/table/index.js
+++ src/components/tasks/table/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from "classnames";
-import { Square } from "react-bootstrap-icons";
+import { Square, CaretDownFill } from "react-bootstrap-icons";
 
 import styles from "./index.module.css";
 
@@ -10,14 +10,32 @@ const Table = ({ data }) => {
       <table>
         <thead>
           <tr>
-            <th></th>
-            <th>Task</th>
-            <th>Priority</th>
-            <th>Due Date</th>
-            <th>Patient</th>
-            <th>Tags</th>
-            <th>Owner</th>
-            <th>Appointed State</th>
+            <th className={styles.checkbox}>
+              <Square />
+            </th>
+            <th className={styles.title}>
+              <span>Task</span>
+            </th>
+            <th>
+              <span>Priority</span>
+              <CaretDownFill />
+            </th>
+            <th>
+              <span>Due Date</span>
+              <CaretDownFill />
+            </th>
+            <th>
+              <span>Patient</span>
+            </th>
+            <th>
+              <span>Tags</span>
+            </th>
+            <th>
+              <span>Owner</span>
+            </th>
+            <th>
+              <span>Appointed State</span>
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -28,10 +46,27 @@ const Table = ({ data }) => {
                   <Square />
                 </td>
                 <td className={styles.title}>{item.title}</td>
-                <td className={styles.priority}>{item.priority}</td>
+                <td className={styles.priority}>
+                  <div
+                    className={classNames(styles.pill, styles[item.priority])}
+                  >
+                    {item.priority}
+                  </div>
+                </td>
                 <td className={styles.duedate}>{item.duedate}</td>
                 <td className={styles.patient}>{item.patient}</td>
-                <td className={styles.tags}>Tags</td>
+                <td className={styles.tags}>
+                  {item.tags.slice(0, 2).map((tag, i) => {
+                    return (
+                      <div key={i} className={styles.tag}>
+                        {tag}
+                      </div>
+                    );
+                  })}
+                  {item.tags.length > 4 && (
+                    <div className={styles.tag}>+{item.tags.length - 2}</div>
+                  )}
+                </td>
                 <td className={styles.owner}>{item.owner}</td>
                 <td className={styles.state}>{item.state}</td>
               </tr>
