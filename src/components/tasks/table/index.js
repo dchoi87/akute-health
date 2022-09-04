c2ca09e53ac1c04e2aff842410fe8b4ebfa75a45
Daniel Choi <dchoi87@gmail.com>
2022-09-03 22:45:52 -0700

    Table Sort

--- src/components/tasks/table/index.js
+++ src/components/tasks/table/index.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { CaretDownFill } from "react-bootstrap-icons";
+import React, { useState } from "react";
+import classNames from "classnames";
+import { ArrowDownShort, ArrowUpShort } from "react-bootstrap-icons";
 
 import Checkbox from "../common/checkbox";
 import Row from "./row";
 
 import { useWindowHeight } from "../hooks/useResize";
-import { useTasksContext } from "../context/tasks";
+import { useFiltersContext } from "../context/filters";
 
 import styles from "./index.module.css";
 
 const Table = ({ tasks, selectedItems, limit }) => {
-  const [, dispatch] = useTasksContext();
+  const [, dispatch] = useFiltersContext();
   const tableHeight = useWindowHeight() - 147 - (selectedItems.length ? 62 : 0);
+  // NOTE: initial state: pull from context or custom hook so that we can unify logic
+  const [sort, setSort] = useState({ priority: "desc" });
+  const [type, setType] = useState("priority");
 
   const handleClick = ({ target }) => {
     const id = target.dataset.id;
@@ -19,6 +23,24 @@ const Table = ({ tasks, selectedItems, limit }) => {
     const payload = id === "all" ? tasks : id;
 
     dispatch({ type, payload });
+  };
+
+  const handleSort = ({ currentTarget }) => {
+    const target = currentTarget.dataset.id;
+    let payload;
+
+    if (type === target) {
+      // set order
+      const order = sort[target] === "asc" ? "desc" : "asc";
+      payload = { ...sort, [target]: order };
+    } else {
+      // change type & order
+      payload = Object.assign({ [target]: sort[target] || "desc" }, sort);
+      setType(target);
+    }
+
+    setSort(payload);
+    dispatch({ type: "CHANGE_SORT", payload: payload });
   };
 
   return (
@@ -38,16 +60,38 @@ const Table = ({ tasks, selectedItems, limit }) => {
             <th className={styles.title}>
               <span>Task</span>
             </th>
-            <th>
+            <th
+              className={classNames(styles.sort, {
+                [styles.selected]: type === "priority",
+              })}
+              data-id="priority"
+              onClick={handleSort}
+            >
               <span>Priority</span>
-              <CaretDownFill />
+              {type === "priority" &&
+                (sort["priority"] === "desc" ? (
+                  <ArrowDownShort />
+                ) : (
+                  <ArrowUpShort />
+                ))}
             </th>
             <th>
               <span>Owner</span>
             </th>
-            <th>
+            <th
+              className={classNames(styles.sort, {
+                [styles.selected]: type === "dueDate",
+              })}
+              data-id="dueDate"
+              onClick={handleSort}
+            >
               <span>Due Date</span>
-              <CaretDownFill />
+              {type === "dueDate" &&
+                (!sort["dueDate"] || sort["dueDate"] === "desc" ? (
+                  <ArrowDownShort />
+                ) : (
+                  <ArrowUpShort />
+                ))}
             </th>
             <th>
               <span>Patient</span>
